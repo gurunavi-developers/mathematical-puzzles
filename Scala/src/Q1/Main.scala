a63583991e8a70b2026a6b6b6f3102c4bee3fc1e
foooomio <foooomio@gmail.com>
2017-02-15 01:26:27 +0900

    Add an answer to Q1 in Scala

--- Scala/src/Q1/Main.scala
+++ Scala/src/Q1/Main.scala
@@ -1,4 +1,14 @@
-object Main extends App{
-  println("Hello World")
+case class Q1(n: Int) {
+  lazy val decimal = n.toString
+  lazy val binary = n.toBinaryString
+  lazy val octal = n.toOctalString
+
+  def forall(f: String => Boolean): Boolean =
+    f(decimal) && f(binary) && f(octal)
 }
 
+val answer = Iterator.from(10, 1).find(
+  Q1(_).forall(s => s == s.reverse)
+).get
+
+println(answer)
