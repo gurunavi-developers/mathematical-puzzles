988669a8c2bb0abfab28b05615ebf56aa3820a88
iwata naoki <a.tokai.wain@gmail.com>
2017-02-15 14:03:13 +0900

    Solve Q1
    - 末尾再帰
    - カリー化

--- Scala/src/Q1/Main.scala
+++ Scala/src/Q1/Main.scala
@@ -1,4 +1,13 @@
 object Main extends App{
-  println("Hello World")
-}
+  def check(n: scala.runtime.RichInt)(f: (scala.runtime.RichInt) => String): Boolean = f(n) == f(n).reverse
 
+  def loop(i: Int): Int = {
+    val c = check(i)(_)
+    if (c { _.toBinaryString } &&
+        c { _.toOctalString } &&
+        c { _.toString } ) i
+    else loop(i + 1)
+  }
+
+  println(loop(10))
+}
